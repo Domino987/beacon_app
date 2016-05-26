259ce2e83c105f75fb439a7ede815946124a01cf
Domino987 <domino051@gmail.com>
2016-05-26 23:19:11 +0200

    26.05.2016 Fragments

--- mobile/src/main/java/de/rwth_aachen/engel/beaconsforproduction/fragment_main.java
+++ mobile/src/main/java/de/rwth_aachen/engel/beaconsforproduction/fragment_main.java
@@ -6,22 +6,24 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.widget.Button;
 
 public class fragment_main extends Fragment implements View.OnClickListener{
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(
                 R.layout.fragment_main, container, false);
-        RelativeLayout machines = (RelativeLayout) view.findViewById(R.id.machinesLayout);
-        RelativeLayout orders = (RelativeLayout) view.findViewById(R.id.ordersLayout);
+        Button machines = (Button) view.findViewById(R.id.machinesButton);
+        Button orders = (Button) view.findViewById(R.id.ordersButton);
         machines.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Fragment fragment = new fragment_orders();
                 FragmentTransaction ft = getFragmentManager().beginTransaction();
                 ft.replace(R.id.fragmentMainContainer, fragment);
-                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                ft.addToBackStack(null);
+                ((Activity_Main)getActivity()).setUpArrow(getString(R.string.orders));
                 ft.commit();
             }
         });
@@ -30,8 +32,10 @@ public class fragment_main extends Fragment implements View.OnClickListener{
             public void onClick(View v) {
                 Fragment fragment = new fragment_machines();
                 FragmentTransaction ft = getFragmentManager().beginTransaction();
-                    ft.replace(R.id.fragmentMainContainer, fragment);
-                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.replace(R.id.fragmentMainContainer, fragment);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                ft.addToBackStack(null);
+                ((Activity_Main)getActivity()).setUpArrow(getString(R.string.machines));
                 ft.commit();
             }
         });
