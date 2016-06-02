b3f7b4409bfb3f297baa1e0a17f5570163b192c9
Domino987 <domino051@gmail.com>
2016-06-02 17:09:50 +0200

    machine Detailview Fix

--- mobile/src/main/java/de/rwth_aachen/engel/beaconsforproduction/Activity_Main.java
+++ mobile/src/main/java/de/rwth_aachen/engel/beaconsforproduction/Activity_Main.java
@@ -14,12 +14,9 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.estimote.sdk.*;
 import com.estimote.sdk.eddystone.Eddystone;
